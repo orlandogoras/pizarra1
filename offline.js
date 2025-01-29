18c39d06a72b49ea625cad6ad1ad9d2290f1a20b
orlandogoras <71925076+orlandogoras@users.noreply.github.com>
2025-01-29 10:16:31 -0500

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1738163617,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/lapiz-sheet0.png",
+		"images/tinta-sheet0.png",
+		"images/borrador-sheet0.png",
+		"images/fondo-sheet0.png",
+		"images/texto1-sheet0.png",
+		"images/boton_procesar-sheet0.png",
+		"media/aplausos.m4a",
+		"media/aplausos.ogg",
+		"media/aplausos_poder.m4a",
+		"media/aplausos_poder.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
